459df7bff57eb5b3c5f3a1c839dbd7aa64117b93
Jacques Favreau <921683+betaorbust@users.noreply.github.com>
2024-12-04 01:48:21 -0800

    Cleaner test output

--- scaffold/next.ts
+++ scaffold/next.ts
@@ -51,7 +51,7 @@ const testCasesPt1: [Parameters<typeof part1>[0], ReturnType<typeof part1>][] =
 	[[\`some input\`, \`some output\`]];
 
 describe('Day ${dayNum}, part 1', () => {
-	test.each(testCasesPt1)('Input: %s. Output: %s', (input, expected) => {
+	test.each(testCasesPt1)('Input:\n%s\nOutput:\n%s', (input, expected) => {
 		expect(part1(input)).toBe(expected);
 	});
 });
@@ -63,7 +63,7 @@ const testCasesPt2: [Parameters<typeof part2>[0], ReturnType<typeof part2>][] =
 	[[\`some input\`, \`some output\`]];
 
 describe('Day ${dayNum}, part 2', () => {
-	test.each(testCasesPt2)('Input: %s. Output: %s', (input, expected) => {
+	test.each(testCasesPt2)('Input:\n%s\nOutput:\n%s', (input, expected) => {
 		expect(part2(input)).toBe(expected);
 	});
 });
