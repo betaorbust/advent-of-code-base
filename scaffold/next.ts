3c54b8913699df2f70de3436d44521dc3925a623
Jacques Favreau <921683+betaorbust@users.noreply.github.com>
2024-12-05 12:57:31 -0800

    Fix newline escape

--- scaffold/next.ts
+++ scaffold/next.ts
@@ -46,7 +46,7 @@ const testCasesPt1: [Parameters<typeof part1>[0], ReturnType<typeof part1>][] =
 	[[\`some input\`, \`some output\`]];
 
 describe('Day ${dayNum}, part 1', () => {
-	test.each(testCasesPt1)('Input:\n%s\nOutput:\n%s', (input, expected) => {
+	test.each(testCasesPt1)('Input:\\n%s\\nOutput:\\n%s', (input, expected) => {
 		expect(part1(input)).toBe(expected);
 	});
 });
@@ -58,7 +58,7 @@ const testCasesPt2: [Parameters<typeof part2>[0], ReturnType<typeof part2>][] =
 	[[\`some input\`, \`some output\`]];
 
 describe('Day ${dayNum}, part 2', () => {
-	test.each(testCasesPt2)('Input:\n%s\nOutput:\n%s', (input, expected) => {
+	test.each(testCasesPt2)('Input:\\n%s\\nOutput:\\n%s', (input, expected) => {
 		expect(part2(input)).toBe(expected);
 	});
 });
